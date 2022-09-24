9a9235b40cdb6bdd914f197abd635dab463cf158
Esraa Hassan <50490824+esraa-mohamed-hassan@users.noreply.github.com>
2022-09-24 20:03:04 +0200

    update adstest

--- tests/Feature/AdsTest.php
+++ tests/Feature/AdsTest.php
@@ -3,6 +3,9 @@
 namespace Tests\Feature;
 
 use App\Models\api\Ads;
+use App\Models\api\Advertisers;
+use App\Models\api\Categories;
+use App\Models\api\Tags;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -14,24 +17,23 @@ class AdsTest extends TestCase
      *
      * @return void
      */
-    public function test_get_ads()
-    {
-        $response = $this->json('GET', '/api/ads')
-            ->assertStatus(200);
 
-        $content = json_decode($response->getContent(), true);
-        $this->assertArrayHasKey('data', $content);
-        $response->assertJson(
-            [
-                'success' => true,
-                'message' => 'All Ads.',
-            ]
-        );
-    }
 
     public function test_create_ads()
     {
-        $payload = [];
+        $advertiser = Advertisers::orderby('created_at', 'desc')->first();
+        $category = Categories::orderby('created_at', 'desc')->first();
+        $tags = Tags::orderby('created_at', 'desc')->first();
+
+        $payload = [
+                    'title' => 'Adv title 1',
+                    'description' => 'Adv description 1',
+                    'advertiser' => $advertiser['id'],
+                    'category' => $category['id'],
+                    'start_date' => '2022-09-25',
+                    'type' => 'free',
+                    'tags' => $tags['id'],
+        ];
 
         $response = $this->json('POST', '/api/ads', $payload)
             ->assertStatus(200);
@@ -42,18 +44,35 @@ class AdsTest extends TestCase
         $response->assertJson(
             [
                 'success' => true,
-                'message' => 'Added new advertiser successfully.',
+                'message' => 'Added new Ads successfully.',
             ]
         );
     }
 
-    public function test_update_ads()
+    public function test_get_ads_advertisers()
     {
-        $advertisers = Ads::get();
-        $id = $advertisers[0]->id;
+        $advertiser = Advertisers::get();
+        $email = $advertiser[0]['email'];
+        $response = $this->json('GET', '/api/ads/advertiser/'.$email )
+            ->assertStatus(200);
+
+        $content = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('data', $content);
+        $response->assertJson(
+            [
+                'success' => true,
+                'message' => "All Ads By Advertisers with email:" . $email . ".",
+            ]
+        );
+    }
+
+    public function test_filter_adsByTag()
+    {
+        $tags = Tags::orderby('created_at', 'desc')->first();
+        $tag_id = $tags['id'];
         $payload = [];
 
-        $response = $this->json('PUT', '/api/ads/' . $id, $payload)
+        $response = $this->json('GET', '/api/ads/tags/' . $tag_id, $payload)
             ->assertStatus(200);
 
         $content = json_decode($response->getContent(), true);
@@ -62,23 +81,23 @@ class AdsTest extends TestCase
         $response->assertJson(
             [
                 'success' => true,
-                'message' => 'Updated ads successfully.',
+                'message' => 'All Ads Filtered by TagID:' . $tag_id . '.',
             ]
         );
     }
 
-    public function test_delete_ads()
+    public function test_filter_adsByCategory()
     {
-        $ads = Ads::get();
-        $id = $ads[0]->id;
-        $response = $this->json('Delete', '/api/ads/' . $id)
+        $ads = Ads::orderby('created_at', 'desc')->first();
+        $category_id = $ads['category'];
+        $response = $this->json('GET', '/api/ads/categories/' . $category_id)
             ->assertStatus(200);
 
 
         $response->assertJson(
             [
                 'success' => true,
-                'message' => 'Deleted ads successfully',
+                'message' => 'All Ads Filtered by Category ID:' . $category_id . '.',
             ]
         );
     }
